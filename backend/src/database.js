ffdf04cad3ac2a289715f23b6fd3625eaa23a9a1
JackelineGois <jackelinedemoraisgois@gmail.com>
2022-08-13 19:43:43 +0100

    criando projeto node instalado, express, nodemon e endepoint para listar estudantes e buscar estudante

---
+++ backend/src/database.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    nome: "Jonathan Melo",
+    email: "desenvolvedor.jonathan@gmail.com",
+    cpf: "988978768978",
+    ra: "89789978",
+  },
+  {
+    nome: "Jonathan Melo",
+    email: "desenvolvedor.jonathan@gmail.com",
+    cpf: "988978768978",
+    ra: "84232423",
+  },
+  {
+    nome: "Jonathan Melo",
+    email: "desenvolvedor.jonathan@gmail.com",
+    cpf: "988978768978",
+    ra: "8214322",
+  },
+  {
+    nome: "Jonathan Melo",
+    email: "desenvolvedor.jonathan@gmail.com",
+    cpf: "988978768978",
+    ra: "89789978",
+  },
+];
