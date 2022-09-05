a8cb6be1a2a175189cec90d69b864bd456e88d34
JackelineGois <jackelinedemoraisgois@gmail.com>
2022-09-05 09:06:20 +0100

    22- Criado o componenete NAvBar e separado o CSS dele

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="main-container">
+      <Navbar />
+      <section class="container">
+        <header class="main-header">Consulta De Alunos</header>
+        <div class="loader"></div>
+        <div class="content-page">
+          <div class="padding-left-right-20">
+            <div class="top-actions">
+              <form id="formSearchStudent" class="form-search">
+                <input type="text" name="searchInput" id="searchInput" />
+                <button> Pesquisar </button>
+              </form>
+              <a class="btn btn-dark" href="studentManager.html">
+                {" "}
+                Cadastrar aluno
+              </a>
+            </div>
+            <table id="studentList" class="table-list">
+              <thead>
+                <tr>
+                  <th>Registro Acadêmico</th>
+                  <th>Nome </th>
+                  <th>CPF</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody> </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
